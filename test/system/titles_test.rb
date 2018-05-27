9d78c1a106b95e52b8db50a20e1adb0a616b1be0
Ignacio Fuenzalida <ignacio.fuenzalida@zetatango.com>
2018-05-27 12:14:48 -0400

    title scaffold

---
+++ test/system/titles_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class TitlesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit titles_url
+  #
+  #   assert_selector "h1", text: "Title"
+  # end
+end
